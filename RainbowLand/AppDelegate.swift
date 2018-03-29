67a0be096dd868791fc2d6b42efbc0ba1f6a8ab3
Thomas Shiflet <tom.shiflet@libertychristian.com>
2018-03-29 10:07:48 -0500

    Initial Commit

---
+++ RainbowLand/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RainbowLand
+//
+//  Created by Tom Shiflet on 3/29/18.
+//  Copyright © 2018 Tom Shiflet. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
